1c685f7da1f11cbc0f3223285eb70e5a528f0e45
Svetlana K <kov_sv@mail.ru>
2022-09-08 17:32:37 +1000

    Add branch lambda

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,31 @@ public class Main {
         System.out.println(people);
         System.out.println();
 
-        Collections.sort(people, new PersonComparator(2));
+        // people.sort(Comparator.comparing(Person::getName).thenComparing(Person::getAge));
+        Collections.sort(people, (o1, o2) -> {
+            int length1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+            int length2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+            int rezLength = length1 - length2;
+            int countWords = 2;
+            if (length1 > countWords && length2 > countWords) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                if (rezLength > 0) {
+                    return 1;
+                } else if (rezLength < 0) {
+                    return -1;
+                } else {
+                    int rezAge = o1.getAge() - o2.getAge();
+                    if (rezAge > 0) {
+                        return 1;
+                    } else if (rezAge < 0) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        });
         System.out.println("Сортировка по возрастанию знатности:");
         System.out.println(people);
     }
